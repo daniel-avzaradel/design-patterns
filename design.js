90e445601aca055a12bcdddaa3ba2c4edcfb707a
Daniel Avzaradel <daniela@cgen.com>
2022-05-21 02:10:43 +0300

    created a function for createPerson with args

--- design.js
+++ design.js
@@ -7,3 +7,14 @@ let personA = createObj();
 let personB = createObj();
 
 console.log(personA, personB);
+
+const createPerson = (first, last) => {
+  let person = {};
+  person.firstName = first;
+  person.lastName = last;
+
+  return `${person.firstName} ${person.lastName}`;
+};
+
+let personC = createPerson('Daniel', 'Avzaradel');
+console.log(personC);
